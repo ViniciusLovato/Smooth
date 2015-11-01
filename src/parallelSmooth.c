2a487a2ce7130974971ba3fd39fe5004aad738b9
Guilherme <gperinazzo@gmail.com>
2015-11-01 19:15:19 -0200

    Optimized version

--- src/parallelSmooth.c
+++ src/parallelSmooth.c
@@ -10,8 +10,7 @@
 #define GREEN 1
 #define RED 2
 
-void applySmooth(IplImage*, IplImage*, int start, int end);
-int calculatePixel(uchar*, int, int , int , int , int , int , int );
+void applySmooth(IplImage*, uchar*, int start, int end);
 
 int main(int argc, char *argv[])
 {
@@ -21,10 +20,6 @@ int main(int argc, char *argv[])
 
     // original image
     IplImage* img = 0;
-
-    // result image
-    IplImage* img_result = 0;
-
     uchar *img_local_data = 0;
 
 
@@ -34,15 +29,7 @@ int main(int argc, char *argv[])
     }
 
     // load an image  
-    img=cvLoadImage(argv[1], CV_LOAD_IMAGE_COLOR);
-    if(!img){
-        printf("Could not load image file: %s\n",argv[1]);
-        exit(0);
-    }
-    // Copy the image to apply the smooth algorithm 
-    img_result = cvCloneImage(img);
-
-    printf("height: %d width: %d\n", img->height, img->width);
+    
     // Initialize MPI and get important variable.
     MPI_Status Stat;
     MPI_Init(&argc, &argv);
@@ -50,57 +37,67 @@ int main(int argc, char *argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &number_of_processes);
 
 
-    // Image size
-    int imageSize = img_result->height*img_result->widthStep;
-    printf("Image size: %d\n", imageSize);
 
+    img=cvLoadImage(argv[1], CV_LOAD_IMAGE_COLOR);
+    if(!img){
+        printf("Could not load image file: %s\n",argv[1]);
+        exit(0);
+    }
+
+    // Image size
+    int imageSize = img->height*img->widthStep;
+    printf("Image size: %d\n", imageSize);
+    fflush(stdout);
     
 
 
     // Calculates the amount of work of each process
-    int workload = img_result->height / number_of_processes;
+    int workload = img->height / number_of_processes;
 
-    int rec_size = workload * img_result->widthStep;
+    int rec_size = workload * img->widthStep;
 
     if(rank == 0){
 
         // Array that contains the final imageData
         printf("Calculating from 0 to %d\n", workload);
-        char *result = (char*)malloc(sizeof(char)*imageSize);
+        char *result = malloc(rec_size);
+        char *extra = 0;
         int total_work = workload * number_of_processes;
 
         // The first process always calculates the first part of the matrix
-        applySmooth(img, img_result, 0, workload);
-      
-        // Collect all the processed data
-        MPI_Gather(img_result->imageData, rec_size, MPI_CHAR, result, rec_size, MPI_CHAR, 0, MPI_COMM_WORLD);
+        applySmooth(img, result, 0, workload);
 
-
-        // add the new imageData array to plot the image
-        img_result->imageData = result;
-
-        if (total_work < img_result->height)
+        if (total_work < img->height)
         {
-            applySmooth(img, img_result, total_work, img_result->height);
-            printf ("Root calculated %d extra rows, from %d to %d\n", img_result->height - total_work, total_work, img_result->height);
+            extra = malloc((img->height - total_work) * img->widthStep);
+            applySmooth(img, extra, total_work, img->height);
+            printf ("Root calculated %d extra rows, from %d to %d\n", img->height - total_work, total_work, img->height);
+            memcpy(img->imageData + rec_size * number_of_processes, extra, (img->height - total_work) * img->widthStep);
         }
-        cvSaveImage("result/result.jpg", img_result, 0);
+
+        // Collect all the processed data
+        MPI_Gather(result, rec_size, MPI_CHAR, img->imageData, rec_size, MPI_CHAR, 0, MPI_COMM_WORLD);
+        
+        cvSaveImage("result/result.jpg", img, 0);
         cvReleaseImage(&img);
    }
     else {
       
         // Each process knows where to start
         int start = rank*workload;
+        char *  result = malloc(rec_size);
         printf("Calculating from %d to %d\n", start, start+workload);
-        printf ("Passing %d bytes %d %d\n", imageSize/number_of_processes, rec_size, workload * img_result->widthStep);
+        printf ("Passing %d bytes %d %d\n", imageSize/number_of_processes, rec_size, workload * img->widthStep);
 
-        applySmooth(img, img_result, start, start+workload);
+
+
+        applySmooth(img, result, start, start+workload);
 
         // The offset is important to avoid any problems while building the
         // image
 
 
-        MPI_Gather(img_result->imageData + rec_size * rank, rec_size, MPI_CHAR, NULL , rec_size, MPI_CHAR, 0, MPI_COMM_WORLD); 
+        MPI_Gather(result, rec_size, MPI_CHAR, NULL , rec_size, MPI_CHAR, 0, MPI_COMM_WORLD); 
     }
 
 
@@ -109,7 +106,8 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void applySmooth(IplImage* img, IplImage* img_result, int start, int end){
+
+void applySmooth(IplImage* img, uchar* img_result, int start, int end){
     uchar *data   = NULL;
     uchar *data_result = NULL;
 
@@ -118,7 +116,7 @@ void applySmooth(IplImage* img, IplImage* img_result, int start, int end){
     // get the data from the copied image
     // we have to work with this one because we dont want to mess up the
     // original file
-    data_result = (uchar *)img_result->imageData;
+    data_result = (uchar *)img_result;
 
     // get the image data
     int height    = img->height;
@@ -132,39 +130,32 @@ void applySmooth(IplImage* img, IplImage* img_result, int start, int end){
 
     // Pixels from the border do have less pixels aronund than the others, we
     // have consider this value while calculating the newValue
-    int value = 0;
+    int value = 0, sum[3];
     int i, j, k, l;
+    int load = end - start;
 
-    #pragma omp parallel for private(i, j)
+
+    #pragma omp parallel for private(i, j, k, l, value, sum)
     //  For each pixel in the image
-    for(i=start;i<end;i++){
+    for(i=0;i<load;i++){
         for(j=0;j<width;j++){
-            // New image values
-            data_result[i*step+j*channels+RED]= calculatePixel(data, i, j, height, width, step, channels, RED);
-            data_result[i*step+j*channels+GREEN]= calculatePixel(data, i, j, height, width, step, channels, GREEN);
-            data_result[i*step+j*channels+BLUE]= calculatePixel(data, i, j, height, width, step, channels, BLUE);
-
+            int sum[3];
+            value = sum[RED] = sum[GREEN] = sum[BLUE] = 0;
+            for(k = i + start -2; k < i + start + 3; k++){
+                for(l = j-2; l < j+3; l++){
+                    // Check if the pixel exists (it may be outside the grid)
+                    if((k > 0) && (k < height) && (l > 0) && (l < width)){
+                        // New image values
+                        sum[RED] += data[k*step+l*channels+RED];
+                        sum[GREEN] += data[k*step+l*channels+GREEN];
+                        sum[BLUE] += data[k*step+l*channels+BLUE];
+                        value++;
+                    }
+                }
+            }
+            data_result[i*step+j*channels+RED]= sum[RED] / value;
+            data_result[i*step+j*channels+GREEN]= sum[GREEN] / value;
+            data_result[i*step+j*channels+BLUE]= sum[BLUE] / value;
         }
     } 
-}
-
-int calculatePixel(uchar* data, int i, int j, int height, int width, int step, int channels, int color){
-
-    int value = 0;
-    int newValue = 0;
-    int k,l;
-    // For each pixel in the matrix 5x5 around the current pixel
-    for(k = i-2; k < i + 3; k++){
-        for(l = j-2; l < j+3; l++){
-
-            // Check if the pixel exists (it may be outside the grid)
-            if((k > 0) && (k < height) && (l > 0) && (l < width)){
-
-                // Adds all the pixel values of the 5x5 matrix
-                newValue = newValue + data[k*step+l*channels+color];
-                value++;
-            }                           
-        }
-    }
-    return newValue/value;
 }
